cb2b255ab4615fcf900ae93f65947104b0edc846
Bowen Yu <stevenybw@hotmail.com>
2019-07-12 17:15:51 +0800

    Add an option to turn off test
    Log likelihood and log perplexity are very expensive

--- src/main/scala/org/apache/spark/pacman/ml/LDAExample.scala
+++ src/main/scala/org/apache/spark/pacman/ml/LDAExample.scala
@@ -6,7 +6,8 @@ import org.apache.spark.ml.classification.{LogisticRegression, LogisticRegressio
 import org.apache.spark.ml.clustering.LDA
 import org.apache.spark.ml.feature.StringIndexer
 import org.apache.spark.ml.{Pipeline, PipelineStage}
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.storage.StorageLevel
 import scopt.OptionParser
 
 import scala.collection.mutable
@@ -14,10 +15,11 @@ import scala.collection.mutable
 object LDAExample {
 
   case class Params(
-                     input: String = null,
-                     dataFormat: String = "libsvm",
-                     K: Int = 10,
-                     maxIter: Int = 100) extends AbstractParams[Params]
+    input: String = null,
+    dataFormat: String = "libsvm",
+    K: Int = 10,
+    maxIter: Int = 100,
+    computeTest: Boolean = false) extends AbstractParams[Params]
 
   def main(args: Array[String]) {
     val defaultParams = Params()
@@ -33,6 +35,9 @@ object LDAExample {
       opt[String]("dataFormat")
         .text("data format: libsvm (default), dense (deprecated in Spark v1.1)")
         .action((x, c) => c.copy(dataFormat = x))
+      opt[Boolean]("computeTest")
+        .text("whether to compute the perplexity of the dataset (which may be very costly)")
+        .action((x, c) => c.copy(computeTest = x))
       arg[String]("<input>")
         .text("input path to labeled examples")
         .required()
@@ -46,6 +51,23 @@ object LDAExample {
     }
   }
 
+  def initStaticScheduling(spark: SparkSession): Unit = {
+    // Stub for forcefully static-scheduling
+    spark.sparkContext.parallelize(0 until 1024, 1024).count()
+    spark.sparkContext.updateExecutorLocations()
+    println(s"Number of executors observed: ${spark.sparkContext.executorLocations.size}")
+  }
+
+  def processDataset(spark: SparkSession, df: DataFrame): Dataset[Row] = {
+    val parallelism = spark.conf.get("spark.default.parallelism", "")
+    println(s"Parallelism is ${parallelism}")
+    if (!parallelism.isEmpty) {
+      df.repartition(parallelism.toInt).persist(StorageLevel.MEMORY_AND_DISK.setStaticScheduling())
+    } else {
+      df.persist(StorageLevel.MEMORY_AND_DISK.setStaticScheduling())
+    }
+  }
+
   def run(params: Params): Unit = {
     val spark = SparkSession
       .builder
@@ -54,18 +76,23 @@ object LDAExample {
 
     println(s"LDAExample with parameters:\n$params")
 
+    initStaticScheduling(spark)
     // Load training and test data and cache it.
-    val dataset = spark.read.format(params.dataFormat)
+    val dataset_i = spark.read.format(params.dataFormat)
       .load(params.input)
+    val dataset = processDataset(spark, dataset_i)
+
 
     // Trans a LDA model
     val lda = new LDA().setK(params.K).setMaxIter(params.maxIter)
     val model = lda.fit(dataset)
 
-    val ll = model.logLikelihood(dataset)
-    val lp = model.logPerplexity(dataset)
-    println(s"The lower bound on the log likelihood of the entire corpus: $ll")
-    println(s"The upper bound on perplexity: $lp")
+    if (params.computeTest) {
+      val ll = model.logLikelihood(dataset)
+      val lp = model.logPerplexity(dataset)
+      println(s"The lower bound on the log likelihood of the entire corpus: $ll")
+      println(s"The upper bound on perplexity: $lp")
+    }
 
     // Describe topics.
     val topics = model.describeTopics(3)
